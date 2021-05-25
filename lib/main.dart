1339a83c6c7d5f146550be8a02bedb03f08cc823
Kira Sokolova <Kyra256@proton.me>
2021-05-25 10:33:35 +0300

    Sort items by title

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  var items = [
+    Item("Password", Icon(Icons.vpn_key)),
+    Item("Name", Icon(Icons.person)),
+    Item("Nickname", Icon(Icons.error)), // TODO: add icon
+  ];
+  items.sort((a, b) => a.title.compareTo(b.title));
+
+  runApp(MyApp(items));
 }
 
 class Item {
   String title;
   Icon icon;
+
   Item(this.title, this.icon);
 }
 
 class MyApp extends StatelessWidget {
-  final List items = [
-    Item("Password", Icon(Icons.vpn_key)),
-    Item("Name", Icon(Icons.person)),
-    Item("Nickname", Icon(Icons.error)), // TODO: add icon
-  ];
+  final List items;
+
+  MyApp(this.items);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Random Values Generator',
       theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-        accentColor: Colors.amberAccent
-      ),
+          primarySwatch: Colors.lightBlue, accentColor: Colors.amberAccent),
       home: Scaffold(
         appBar: AppBar(
           title: Text("Random Values Generator"),
