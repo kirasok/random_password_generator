0d508c5e45aaf722c78335ab67ef54eccbeaadb9
Kirill Mokretsov <Kirill.Mokretsov@protonmail.com>
2021-05-26 12:06:09 +0300

    Rapair tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:random_values_generator/main.dart';
+import 'package:random_password_generator/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
